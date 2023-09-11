232ff72b2065a37a9f42887ded014a5877f2e9e8
Chase Callahan <ctcallahan2@crimson.ua.edu>
2023-09-10 22:21:42 -0500

    Finished Solution

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string currSeason = GetSeason();
+
+string message = GetSeasonMessage(currSeason);
+
+System.Console.WriteLine(message);
+
+static string GetSeason(){
+    System.Console.WriteLine("What season is it? (Winter, Spring, Summer, Fall)");
+    
+    // Convert to lowecase to make comparison easier
+    return Console.ReadLine().ToLower();
+}
+
+static string GetSeasonMessage(string season){
+    string message;
+
+    if(season == "fall"){
+         message = FavoriteSeasonMessage("Sherlenthia");
+    }
+    else if(season == "winter"){
+        message = FavoriteSeasonMessage("Manav");           
+    } 
+    else{
+        message = "It is no ones favorite season";
+    }
+
+    return message;
+}
+
+static string FavoriteSeasonMessage(string name){
+    return $"It is ${name}'s favoite season!";
+}
